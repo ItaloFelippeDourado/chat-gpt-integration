cc2372f0dc31d018d1a4b86483db753ccef7f669
Ítalo Felippe Dourado <50452678+ItaloFelippeDourado@users.noreply.github.com>
2025-05-14 10:04:53 -0300

    Initial Commit

---
+++ src/main/java/metaldome/com/br/chat_gpt_integration/ChatGptIntegrationApplication.java
@@ -0,0 +1,13 @@
+package metaldome.com.br.chat_gpt_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/metaldome/com/br/chat_gpt_integration/ChatGptIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package metaldome.com.br.chat_gpt_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
